0111f3194650aff764120e5b55aa7738f689e7e9
Glo Umutoni <gumutoni@g.harvard.edu>
2024-11-08 20:21:13 -0500

    dataset updates ...initial set

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-echo "Container is running!!!"
+echo 'Container is running!!!'
 
-
+# Authenticate with Google Cloud
 gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
-mkdir -p /mnt/gcs_bucket
+
+# Create mount point with consistent naming
+mkdir -p /mnt/gcs_data
+
+# Mount the GCS bucket
 gcsfuse --key-file=$GOOGLE_APPLICATION_CREDENTIALS $GCS_BUCKET_NAME /mnt/gcs_data
 echo 'GCS bucket mounted at /mnt/gcs_data'
-mkdir -p /app/cheese_dataset
-mount --bind /mnt/gcs_data/images /app/cheese_dataset
 
+# Create directory for cheese dataset
+mkdir -p /app/cheese_dataset
+
+# Mount the unlabeled cheeses directory
+mount --bind /mnt/gcs_data/cheeses_unlabeled /app/cheese_dataset
+echo 'Unlabeled cheese images mounted at /app/cheese_dataset'
+
+# Activate virtual environment
 pipenv shell
--- docker-shell.sh
+++ docker-shell.sh
@@ -4,9 +4,9 @@ set -e
 
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCS_BUCKET_NAME="cheese-app-data-versioning"
-export GCP_PROJECT="ac215-project"
-export GCP_ZONE="us-central1-a"
+export GCS_BUCKET_NAME="cheese-app-demo-glo"
+export GCP_PROJECT="ac215"
+export GCP_ZONE="US"
 export GOOGLE_APPLICATION_CREDENTIALS="/secrets/data-service-account.json"
 
 
